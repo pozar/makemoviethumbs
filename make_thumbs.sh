01466245930fef92eef1b16f575af4de2d1a7173
Tim Pozar <github@lns.com>
2016-07-25 10:44:55 -0700

    Added bunch of stuff...
    command line options as well as a set number of thumbs averaged over the start/stop times or duration of the film.

--- make_thumbs.sh
+++ make_thumbs.sh
@@ -1,15 +1,73 @@
 #!/bin/bash
 
-inputfile="TheWaspWoman1959"
-inputext="mp4"
-start=60
-end=300
-interval_secs=5
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-# Get rid of previous thumbnails..
-rm *.jpeg
+# Initialize our own variables:
+inputfile=""
+interval_secs=0
+starttime=0
+endtime=0
+average=0
+
+show_help ()
+{
+    echo "$0:"
+    echo "   -h (show help)"
+    echo "   -v (verbose)"
+    echo "   -f inputfile"
+    echo "   -i interval_secs"
+    echo "   -s starttime_secs"
+    echo "   -e endtime_secs"
+    echo "   -a total_images_averaged_over_the_duration_or_start_stop_time"
+}
+
+while getopts "h?f:i:s:e:a:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    f)  inputfile=$OPTARG
+        ;;
+    i)  interval_secs=$OPTARG
+        ;;
+    s)  starttime=$OPTARG
+        ;;
+    e)  endtime=$OPTARG
+        ;;
+    a)  average=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+if [ -z $inputfile ]; then
+   echo We need a filename
+   show_help
+   exit 1
+fi
+
+if [ $endtime -eq 0 ]; then
+   # When used to calculate average interverals or end time is not defined...
+   endtime=`ffprobe -i $inputfile -show_entries format=duration -v quiet -of csv="p=0" | sed 's/\..*//'`
+fi
+
+if [ $average -gt 0 ]; then
+   let duration=endtime-starttime
+   let interval_secs=duration/average
+fi
+
+# Get rid of previous thumbnails and don't error if it doesn't find anything
+rm *.jpeg || true
 # Create sequence...
-ffmpeg -loglevel panic -i $inputfile.$inputext -ss $start -t $end -f image2 -qscale 2 -vf fps=1/$interval_secs %d.jpeg
+ffmpeg -loglevel panic -i $inputfile -ss $starttime -t $endtime -f image2 -qscale 2 -vf fps=1/$interval_secs %d.jpeg
+
+# Drop off the filename extesion...
+inputfile="${inputfile/.mp4/}"
 # Get a listing of the thumbnail filenames...
 arr=( $(ls *.jpeg) )
 # Number of filenames
@@ -18,7 +76,7 @@ qnt=${#arr[@]}
 i=1
 while [ $i -le $qnt ]; do
     let totalsec=i*interval_secs
-    let timestampsecs=totalsec+start
+    let timestampsecs=totalsec+starttime
     let sec=(timestampsecs % 60)
     let min=(timestampsecs % 3600)/60
     let hour=timestampsecs/3600
